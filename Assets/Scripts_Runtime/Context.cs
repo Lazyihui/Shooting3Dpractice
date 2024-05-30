1c651dd023bacf33447092e5e586b47ee44b7bb4
包子 <chengbaozi6@qq.com>
2024-05-30 23:49:23 +0800

    <feature>add:ModuleAssets

---
+++ Assets/Scripts_Runtime/Context.cs
@@ -0,0 +1,13 @@
+
+
+public class Context {
+
+    public AssetsContext assetsContext;
+
+
+    public Context() {
+        assetsContext = new AssetsContext();
+    }
+
+    public void Inject() { }
+}
--- Assets/Scripts_Runtime/Main.cs
+++ Assets/Scripts_Runtime/Main.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
+
+    Context ctx;
+    bool isTearDown = false;
+
     void Awake() {
+        // === init===
+        ctx = new Context();
+        // === Load===
+        ModuleAssets.Load(ctx.assetsContext);
         Debug.Log("Main Awake");
+
+
     }
     // Update is called once per frame
     void Update() {
 
+    }
+
+
+    void OnDestory() {
+        TearDown();
+    }
+
+    void OnApplicationQuit() {
+        TearDown();
+    }
+
+    void TearDown() {
+        if (isTearDown) {
+            return;
+        }
+        isTearDown = true;
+        // === Unload===
+        ModuleAssets.Unload(ctx.assetsContext);
     }
 }
